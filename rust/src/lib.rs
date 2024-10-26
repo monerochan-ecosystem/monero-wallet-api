0cf23f791de29b28f8817ce973b06e61bcada1a4
Spirobel <spirobel@protonmail.com>
2024-10-27 05:45:44 +0800

    WIP initialize viewpair + wasi browser wrapper

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,49 +1,22 @@
 use std::cell::RefCell;
 
+use curve25519_dalek::scalar::Scalar;
+use hex::FromHex;
+use monero_wallet;
+use monero_wallet::ViewPair;
+use zeroize::Zeroizing;
 thread_local! {
-    static THREAD_LOCAL_STRING: RefCell<String> = RefCell::new(String::from("Initial value"));
-}
-pub fn add(left: usize, right: usize) -> usize {
-    THREAD_LOCAL_STRING.with(|string| {
-        println!("Initial value: {}", string.borrow());
-
-        // Modify the string
-        *string.borrow_mut() = String::from("New value");
-
-        println!("Modified value: {}", string.borrow());
-    });
-
-    left + right
+    static GLOBAL_VIEWPAIR: RefCell<Option<ViewPair>> = RefCell::new(None);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use curve25519_dalek::scalar::Scalar;
-    use hex::FromHex;
-    use monero_wallet;
-    use zeroize::Zeroizing;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn test_monero_serai() {
-        THREAD_LOCAL_STRING.with(|string| {
-            println!("Initial value: {}", string.borrow());
-
-            // Modify the string
-            *string.borrow_mut() = String::from("New value");
-
-            println!("Modified value: {}", string.borrow());
-        });
+#[no_mangle]
+pub extern "C" fn init_view_pair() {
+    GLOBAL_VIEWPAIR.with(|old_viewpair| {
+        let mut viewpair = old_viewpair.borrow_mut();
         let primary_address = "55Py9fSwyEeQX1CydtFfPk96uHEFxSxvD9AYBy7dwnYt9cXqKDjix9rS9AWZ5GnH4B1Z7yHr3B2UH2updNw5ZNJEEnv87H1";
         let secret_view_key = "1195868d30373aa9d92c1a21514de97670bcd360c209a409ea3234174892770e";
         let view_key_bytes = <[u8; 32]>::from_hex(secret_view_key.to_string()).unwrap();
-        let viewpair = monero_wallet::ViewPair::new(
+         *viewpair = Some(monero_wallet::ViewPair::new(
             monero_wallet::address::MoneroAddress::from_str_with_unchecked_network(primary_address)
                 .map_err(|e| {
                     eprintln!(
@@ -62,11 +35,26 @@ mod tests {
                                                                                    //     std::process::exit(1);
                                                                                    // }).unwrap(),
         )
-        .unwrap();
-        println!(
-            "HI {:#?}",
-            viewpair.legacy_address(monero_wallet::address::Network::Stagenet)
-        );
+        .unwrap());
+
+        // Check if ViewPair is present and print its legacy address
+        if let Some(vp) = &*viewpair {
+            println!(
+                "HI {:#?}",
+                vp.legacy_address(monero_wallet::address::Network::Stagenet)
+            );
+        } else {
+            eprintln!("ViewPair has not been initialized");
+        }
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_monero_serai() {
         let result = 4;
         assert_eq!(result, 4);
     }
