ca047d059640025595d190b14f2dfee17f999eb6
Spirobel <spirobel@protonmail.com>
2024-10-30 04:22:45 +0800

    WIP flesh out the FFI API

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
+use core::str;
 use std::cell::RefCell;
 
 use curve25519_dalek::scalar::Scalar;
@@ -6,17 +7,65 @@ use monero_wallet;
 use monero_wallet::ViewPair;
 use serde_json::Value;
 use std::io::{self, Read};
+use your_program::{input, input_string, output_string};
 use zeroize::Zeroizing;
 thread_local! {
     static GLOBAL_VIEWPAIR: RefCell<Option<ViewPair>> = RefCell::new(None);
 }
+mod your_program {
+    /// implement input & output in your program to share arrays with the monero-wallet-api
+    /// rust will take care of allocation and deallocation
+    mod yours {
+        extern "C" {
+            pub fn input(ptr: *mut u8, length: usize);
+        }
+        extern "C" {
+            pub fn output(ptr: *const u8, length: usize);
+        }
+    }
+    /// internal wrappers to handle input and output of strings
+    pub fn input(length: usize) -> Vec<u8> {
+        let mut vec = Vec::with_capacity(length);
+
+        unsafe {
+            yours::input(vec.as_mut_ptr(), length);
+        }
+
+        vec
+    }
+    pub fn output(value: &Vec<u8>) {
+        unsafe { yours::output(value.as_ptr(), value.len()) };
+    }
+    pub fn input_string(length: usize) -> String {
+        let mut vec = Vec::with_capacity(length);
+
+        unsafe {
+            yours::input(vec.as_mut_ptr(), length);
+            return String::from_utf8_unchecked(vec);
+        }
+    }
+    pub fn output_string(value: &str) {
+        unsafe { yours::output(value.as_ptr(), value.len()) };
+    }
+}
+/// WASM / C ABI
 //TODO init view pair with name (hashmap of view pairs)
 #[no_mangle]
 pub extern "C" fn init_view_pair() {
     GLOBAL_VIEWPAIR.with(|old_viewpair| {
         //TODO let library consumer pass in a function to handle input
-        let mut buf = String::new();
-        io::stdin().read_to_string(&mut buf);
+            // let mut result = String::with_capacity(len_str.len());
+            // result.as_ptr()
+            // String::from_utf8_unchecked(bytes)
+// println!("Result string: {}", unsafe { str::from_utf8_unchecked(&input(1)[..]) });  
+//         let result_str = unsafe { str::from_utf8_unchecked(input(100).as_slice()) };
+//         input(1);
+//             let mut buf = vec![0; 100];
+//         io::stdin().read(&mut buf);
+        //io::stdin().read_to_string(&mut buf);
+        init_view_pair2();
+        let prim = input_string(10);
+        output_string(prim.as_str());
         let mut viewpair = old_viewpair.borrow_mut();
         let primary_address = "55Py9fSwyEeQX1CydtFfPk96uHEFxSxvD9AYBy7dwnYt9cXqKDjix9rS9AWZ5GnH4B1Z7yHr3B2UH2updNw5ZNJEEnv87H1";
         let secret_view_key = "1195868d30373aa9d92c1a21514de97670bcd360c209a409ea3234174892770e";
@@ -54,7 +103,8 @@ pub extern "C" fn init_view_pair() {
         }
     });
 }
-
+///rust API
+pub fn init_view_pair2() {}
 #[cfg(test)]
 mod tests {
     use super::*;
