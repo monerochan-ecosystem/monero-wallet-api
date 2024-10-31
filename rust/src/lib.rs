3c399c43c530806f647d3bc2a21ecbf9514ef805
Spirobel <spirobel@protonmail.com>
2024-10-31 11:40:16 +0800

    WIP wasi ffi bindings + wrappers

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -51,47 +51,17 @@ mod your_program {
 /// WASM / C ABI
 //TODO init view pair with name (hashmap of view pairs)
 #[no_mangle]
-pub extern "C" fn init_view_pair() {
-    GLOBAL_VIEWPAIR.with(|old_viewpair| {
-        //TODO let library consumer pass in a function to handle input
-            // let mut result = String::with_capacity(len_str.len());
-            // result.as_ptr()
-            // String::from_utf8_unchecked(bytes)
-// println!("Result string: {}", unsafe { str::from_utf8_unchecked(&input(1)[..]) });  
-//         let result_str = unsafe { str::from_utf8_unchecked(input(100).as_slice()) };
-//         input(1);
-//             let mut buf = vec![0; 100];
-//         io::stdin().read(&mut buf);
-        //io::stdin().read_to_string(&mut buf);
-        init_view_pair2();
-        let prim = input_string(10);
-        output_string(prim.as_str());
-        let mut viewpair = old_viewpair.borrow_mut();
-        let primary_address = "55Py9fSwyEeQX1CydtFfPk96uHEFxSxvD9AYBy7dwnYt9cXqKDjix9rS9AWZ5GnH4B1Z7yHr3B2UH2updNw5ZNJEEnv87H1";
-        let secret_view_key = "1195868d30373aa9d92c1a21514de97670bcd360c209a409ea3234174892770e";
-        let view_key_bytes = <[u8; 32]>::from_hex(secret_view_key.to_string()).unwrap();
-         *viewpair = Some(monero_wallet::ViewPair::new(
-            monero_wallet::address::MoneroAddress::from_str_with_unchecked_network(primary_address)
-                .map_err(|e| {
-                    eprintln!(
-                        "There is an issue with the primary address that you provided: {}",
-                        primary_address.to_string()
-                    );
-                    eprintln!("{}", e.to_string());
-                    std::process::exit(1);
-                })
-                .unwrap()
-                .spend(),
-            Zeroizing::new(Scalar::from_canonical_bytes(view_key_bytes).unwrap()), // monero::PrivateKey::from_str(secret_view_key).map_err(|e| {
-                                                                                   //     eprintln!("There is an issue with the view key that you provided: {}",
-                                                                                   //       secret_view_key.to_string());
-                                                                                   //     eprintln!("{}", e.to_string());
-                                                                                   //     std::process::exit(1);
-                                                                                   // }).unwrap(),
-        )
-        .unwrap());
+pub extern "C" fn init_viewpair(
+    primary_address_string_len: usize,
+    secret_view_key_string_len: usize,
+) {
+    let primary_address = &input_string(primary_address_string_len);
+    let secret_view_key = &input_string(secret_view_key_string_len);
 
-        //TODO let library consumer pass in a function to handle output
+    GLOBAL_VIEWPAIR.with(|old_viewpair| {
+        let mut viewpair = old_viewpair.borrow_mut();
+        *viewpair = Some(make_viewpair(primary_address, secret_view_key));
+
         // Check if ViewPair is present and print its legacy address
         if let Some(vp) = &*viewpair {
             println!(
@@ -104,7 +74,24 @@ pub extern "C" fn init_view_pair() {
     });
 }
 ///rust API
-pub fn init_view_pair2() {}
+pub fn make_viewpair(primary_address: &str, secret_view_key: &str) -> ViewPair {
+    let view_key_bytes = <[u8; 32]>::from_hex(secret_view_key.to_string()).unwrap();
+    monero_wallet::ViewPair::new(
+        monero_wallet::address::MoneroAddress::from_str_with_unchecked_network(primary_address)
+            .map_err(|e| {
+                eprintln!(
+                    "There is an issue with the primary address that you provided: {}",
+                    primary_address.to_string()
+                );
+                eprintln!("{}", e.to_string());
+                std::process::exit(1);
+            })
+            .unwrap()
+            .spend(),
+        Zeroizing::new(Scalar::from_canonical_bytes(view_key_bytes).unwrap()),
+    )
+    .unwrap()
+}
 #[cfg(test)]
 mod tests {
     use super::*;
