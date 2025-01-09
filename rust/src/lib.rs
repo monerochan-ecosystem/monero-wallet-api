811d81c03d79ff855d28f96f1f7654a6c1690d1e
Spirobel <spirobel@protonmail.com>
2025-01-09 19:48:39 +0800

    no explosion if there is no ViewPair for getBlocksBin Response parsing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -133,98 +133,103 @@ pub extern "C" fn parse_response(response_len: usize) {
     // })
     // Now you can use view_pair_ref
     GLOBAL_SCANNER.with(|old_scanner| {
-        let global_scanner = old_scanner.borrow();
-        let mut scanner = global_scanner.clone().unwrap();
-
-        println!(
-            "Processing normal response: {:?}",
-            blocks_response.output_indices[0].indices[0].indices[0]
-        );
-        println!(
-            "Processing normal response: {:?}",
-            blocks_response.output_indices[1].indices[0]
-        );
-        println!(
-            "Processing normal response: {:?}",
-            blocks_response.output_indices[3].indices[0]
-        );
-        //  println!("Processing normal response: {:?}", response);
-        for (index, block_entry) in blocks_response.blocks.iter().enumerate() {
-            let output_index_for_first_ringct_output =
-                Some(blocks_response.output_indices[index].indices[0].indices[0]);
-            // Process each block here
-            let blockhihi = Block::read::<&[u8]>(&mut block_entry.block.as_ref()).unwrap();
-            // println!("Processing block: {:?}", blockhihi.miner_transaction);
-            //  output_index_for_first_ringct_output +=
-            //  u64::try_from(tx.prefix().outputs.len()).unwrap();
-            let mut transactions = Vec::new();
-
-            match &block_entry.txs {
-                TransactionBlobs::Normal(txs) => {
-                    println!("Processing normal transaction: {:?}", txs);
-                    for tx_bytes in txs {
-                        let tx = Transaction::<NotPruned>::read(&mut tx_bytes.as_ref());
-                        // transactions.push(tx);
-                        // let tx =
-                        //     Transaction::<Pruned>::read(tx_bytes).map_err(
-                        //         |_| match hash_hex(&res.tx_hash) {
-                        //             Ok(hex_hash) => Error::InvalidTransaction(format!(
-                        //                 "Failed to parse transaction: {}",
-                        //                 hex_hash
-                        //             )),
-                        //             Err(_) => Error::InvalidTransaction(
-                        //                 "Failed to generate transaction hash".to_string(),
-                        //             ),
-                        //         },
-                        //     );
-
-                        // if let Ok(parsed_tx) = tx {
-                        //     parsed_transactions.push(parsed_tx);
-                        // } else {
-                        //     // Handle error case, possibly skip this transaction
-                        //     println!("Warning: Skipping invalid transaction");
-                        // }
-                        println!("Processing normal transaction: {:?}", tx);
-                        // println!("Processing block: {:?}", blockhihi);
-                    }
-                }
-                TransactionBlobs::Pruned(pruned_txs) => {
-                    // Handle pruned transactions separately
-                    //println!("Processing pruned transaction: {:?}", pruned_txs[0]);
-                    // let blockhihi =
-                    //     Block::read::<&[u8]>(&mut block_entry.block.as_ref()).unwrap();
-                    //  println!("Processing block: {:?}", blockhihi);
-                    for entry in pruned_txs {
-                        // Process PrunedTxBlobEntry here
-                        let tx =
-                            Transaction::<Pruned>::read::<&[u8]>(&mut entry.blob.as_ref()).unwrap();
-                        transactions.push(tx);
-                        //println!("Processing pruned transaction: {:?}", tx);
-                        // Add parsing logic for pruned transactions if needed
-                    }
-                }
-                TransactionBlobs::None => {
-                    //     println!("No transactions in this block");
-                }
+        match old_scanner.borrow().clone() {
+            None => {
+                println!("Scanner is not initialized. Skipping processing.");
+                return;
             }
+            Some(mut scanner) => {
+                println!(
+                    "Processing normal response: {:?}",
+                    blocks_response.output_indices[0].indices[0].indices[0]
+                );
+                println!(
+                    "Processing normal response: {:?}",
+                    blocks_response.output_indices[1].indices[0]
+                );
+                println!(
+                    "Processing normal response: {:?}",
+                    blocks_response.output_indices[3].indices[0]
+                );
+                //  println!("Processing normal response: {:?}", response);
+                for (index, block_entry) in blocks_response.blocks.iter().enumerate() {
+                    let output_index_for_first_ringct_output =
+                        Some(blocks_response.output_indices[index].indices[0].indices[0]);
+                    // Process each block here
+                    let blockhihi = Block::read::<&[u8]>(&mut block_entry.block.as_ref()).unwrap();
+                    // println!("Processing block: {:?}", blockhihi.miner_transaction);
+                    //  output_index_for_first_ringct_output +=
+                    //  u64::try_from(tx.prefix().outputs.len()).unwrap();
+                    let mut transactions = Vec::new();
 
-            let scanBlock = ScannableBlock {
-                block: blockhihi,
-                transactions,
-                output_index_for_first_ringct_output,
-            };
-            let res = scanner.scan(scanBlock).unwrap().not_additionally_locked();
-            println!("weijokfjiweioewioewioeweeeeeeeeeeee {:?}", res.len());
-            // res.
-            for x in res {
-                println!("hi there {:?}", x.commitment());
-                // match serde_json::to_string(&x.) {
-                //     Ok(json_string) => println!("{}", json_string),
-                //     Err(e) => eprintln!("Serialization error: {}", e),
-                // }
+                    match &block_entry.txs {
+                        TransactionBlobs::Normal(txs) => {
+                            println!("Processing normal transaction: {:?}", txs);
+                            for tx_bytes in txs {
+                                let tx = Transaction::<NotPruned>::read(&mut tx_bytes.as_ref());
+                                // transactions.push(tx);
+                                // let tx =
+                                //     Transaction::<Pruned>::read(tx_bytes).map_err(
+                                //         |_| match hash_hex(&res.tx_hash) {
+                                //             Ok(hex_hash) => Error::InvalidTransaction(format!(
+                                //                 "Failed to parse transaction: {}",
+                                //                 hex_hash
+                                //             )),
+                                //             Err(_) => Error::InvalidTransaction(
+                                //                 "Failed to generate transaction hash".to_string(),
+                                //             ),
+                                //         },
+                                //     );
+
+                                // if let Ok(parsed_tx) = tx {
+                                //     parsed_transactions.push(parsed_tx);
+                                // } else {
+                                //     // Handle error case, possibly skip this transaction
+                                //     println!("Warning: Skipping invalid transaction");
+                                // }
+                                println!("Processing normal transaction: {:?}", tx);
+                                // println!("Processing block: {:?}", blockhihi);
+                            }
+                        }
+                        TransactionBlobs::Pruned(pruned_txs) => {
+                            // Handle pruned transactions separately
+                            //println!("Processing pruned transaction: {:?}", pruned_txs[0]);
+                            // let blockhihi =
+                            //     Block::read::<&[u8]>(&mut block_entry.block.as_ref()).unwrap();
+                            //  println!("Processing block: {:?}", blockhihi);
+                            for entry in pruned_txs {
+                                // Process PrunedTxBlobEntry here
+                                let tx =
+                                    Transaction::<Pruned>::read::<&[u8]>(&mut entry.blob.as_ref())
+                                        .unwrap();
+                                transactions.push(tx);
+                                //println!("Processing pruned transaction: {:?}", tx);
+                                // Add parsing logic for pruned transactions if needed
+                            }
+                        }
+                        TransactionBlobs::None => {
+                            //     println!("No transactions in this block");
+                        }
+                    }
+
+                    let scanBlock = ScannableBlock {
+                        block: blockhihi,
+                        transactions,
+                        output_index_for_first_ringct_output,
+                    };
+                    let res = scanner.scan(scanBlock).unwrap().not_additionally_locked();
+                    println!("weijokfjiweioewioewioeweeeeeeeeeeee {:?}", res.len());
+                    // res.
+                    for x in res {
+                        println!("hi there {:?}", x.commitment());
+                        // match serde_json::to_string(&x.) {
+                        //     Ok(json_string) => println!("{}", json_string),
+                        //     Err(e) => eprintln!("Serialization error: {}", e),
+                        // }
+                    }
+                }
             }
         }
-
         //  scanner.scan(block)
     });
 }
