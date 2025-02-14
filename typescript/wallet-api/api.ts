15f566d585155f56bc5adc96b5c465ebe6368e3c
Spirobel <spirobel@protonmail.com>
2025-02-15 02:02:57 +0800

    add documentation

--- typescript/wallet-api/api.ts
+++ typescript/wallet-api/api.ts
@@ -12,6 +12,13 @@ import { WasmProcessor } from "./wasm-processing/wasmProcessor";
 export * from "./node-interaction/binaryEndpoints";
 export * from "./node-interaction/jsonEndpoints";
 export type ScanResultCallback = (result: ScanResult | ErrorResponse) => void;
+/**
+ * This class is useful to interact with Moneros DaemonRpc binary requests in a convenient way.
+ * (similar to how you would interact with a REST api that gives you json back.)
+ * The wasm part will handle the creation of the binary requests and parse the responses and then parse them
+ * and return outputs that belong to the ViewPair.
+ * {@link https://docs.getmonero.org/rpc-library/monerod-rpc/#get_blocksbin}
+ */
 export class ViewPair extends WasmProcessor {
   public static async create(
     primary_address: string,
@@ -37,7 +44,11 @@ export class ViewPair extends WasmProcessor {
     return viewPair;
   }
   /**
-   *
+   * This request helps making requests to the get_blocks.bin endpoint of the Monerod nodes.
+   * @link https://docs.getmonero.org/rpc-library/monerod-rpc/#get_blocksbin
+   * @param params params that will be turned into epee (moner lib that does binary serialization)
+   * @param metaCallBack contains meta information about the getBlocksbin call (new sync height = start_height param + number of blocks)
+   * @returns The difference to the same method on NodeUrl is: It returns {@link ScanResult} (outputs that belong to viewpair) and not just the blocks as json.
    */
   public getBlocksBin(
     params: GetBlocksBinRequest,
@@ -46,7 +57,12 @@ export class ViewPair extends WasmProcessor {
     return getBlocksBinScan(this, params, metaCallBack);
   }
   /**
-   * scan
+   * This method will use getBlocks.bin from start height to daemon height.
+   * This is CPU bound work, so it should be executed in a seperate thread (worker).
+   * The scanner.ts worker in the standard-checkout dir shows how to keep scanning after the tip is reached.
+   * It also shows how the outputs are saved (note the unqiue requirement for the stealth_adress).
+   * @param start_height the height to start syncing from.
+   * @param callback this function will get the new outputs as they are found as a parameter
    */
   public async scan(start_height: number, callback: ScanResultCallback) {
     let latest_meta: GetBlocksResultMeta = {
@@ -66,6 +82,12 @@ export class ViewPair extends WasmProcessor {
       callback(res);
     }
   }
+  /**
+   * This method makes an integrated Address for the Address of the Viewpair it was opened with.
+   * The network (mainnet, stagenet, testnet) is the same as the one of the Viewpairaddress.
+   * @param paymentId (u64 under the hood) you can use a random number or a primary key of an sqlite db to associate payments with customer sessions.
+   * @returns Adressstring
+   */
   public makeIntegratedAddress(paymentId: number) {
     let address = "";
     this.readFromWasmMemory = (ptr, len) => {
@@ -76,7 +98,12 @@ export class ViewPair extends WasmProcessor {
     return address;
   }
 }
-
+/**
+ * This class is useful to interact with Moneros DaemonRpc binary requests in a convenient way.
+ * (similar to how you would interact with a REST api that gives you json back.)
+ * The wasm part will handle the creation of the binary requests and parse the responses and return them as json.
+ * {@link https://docs.getmonero.org/rpc-library/monerod-rpc/#get_blocksbin}
+ */
 export class NodeUrl extends WasmProcessor {
   public static async create(node_url?: string): Promise<NodeUrl> {
     const nodeUrl = new NodeUrl(
@@ -87,7 +114,10 @@ export class NodeUrl extends WasmProcessor {
     return nodeUrl;
   }
   /**
-   *
+   * This request helps making requests to the get_blocks.bin endpoint of the Monerod nodes.
+   *  @link https://docs.getmonero.org/rpc-library/monerod-rpc/#get_blocksbin
+   * @param params params that will be turned into epee (moner lib that does binary serialization)
+   * @returns after the request is made it will return epee serialized objects that are then parsed into json.
    */
   public getBlocksBin(params: GetBlocksBinRequest) {
     return getBlocksBinJson(this, params);
