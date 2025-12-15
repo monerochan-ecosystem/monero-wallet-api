3baf3934ca90f253867fd25818e24020b3dcfeb9
Spirobel <spirobel@protonmail.com>
2025-12-15 13:19:53 +0800

    starting from the Bun.file and Bun.write interface + skeleton class, we can fill in the platforms we want to run in. This is for indexedDB, but Android local app storage (DataStore) will work the same way. The result is, that all the code above, that depends on Bun.file and Bun.write will just work in React Native as well.

---
+++ typescript/wallet-api/io/BunFileInterface.ts
@@ -0,0 +1,51 @@
+export interface Bun {
+  stdin: BunFile;
+  stdout: BunFile;
+  stderr: BunFile;
+
+  file(path: string | number | URL, options?: { type?: string }): BunFile;
+
+  write(
+    destination: string | number | BunFile | URL,
+    input:
+      | string
+      | Blob
+      | ArrayBuffer
+      | SharedArrayBuffer
+      | TypedArray
+      | Response
+  ): Promise<number>;
+}
+
+export interface BunFile {
+  readonly size: number;
+  readonly type: string;
+
+  text(): Promise<string>;
+  stream(): ReadableStream;
+  arrayBuffer(): Promise<ArrayBuffer>;
+  json(): Promise<any>;
+  writer(params: { highWaterMark?: number }): FileSink;
+  exists(): Promise<boolean>;
+}
+
+export interface FileSink {
+  write(
+    chunk: string | ArrayBufferView | ArrayBuffer | SharedArrayBuffer
+  ): number;
+  flush(): number | Promise<number>;
+  end(error?: Error): number | Promise<number>;
+  start(options?: { highWaterMark?: number }): void;
+  ref(): void;
+  unref(): void;
+}
+export type TypedArray =
+  | Int8Array
+  | Uint8Array
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array
+  | Uint8ClampedArray;
---
+++ typescript/wallet-api/io/indexedDB.ts
@@ -0,0 +1,91 @@
+import type { BunFile, FileSink, Bun, TypedArray } from "./BunFileInterface";
+class IndexedDBBun implements Bun {
+  stdin: BunFile = new IndexedDBFile();
+  stdout: BunFile = new IndexedDBFile();
+  stderr: BunFile = new IndexedDBFile();
+
+  file(path: string | number | URL, options?: { type?: string }): BunFile {
+    throw new Error("not implemented");
+    return new IndexedDBFile();
+  }
+
+  async write(
+    destination: string | number | BunFile | URL,
+    input:
+      | string
+      | Blob
+      | ArrayBuffer
+      | SharedArrayBuffer
+      | TypedArray
+      | Response
+  ): Promise<number> {
+    throw new Error("not implemented");
+    return 0;
+  }
+}
+
+class IndexedDBFile implements BunFile {
+  readonly size: number = 0;
+  readonly type: string = "";
+
+  text(): Promise<string> {
+    throw new Error("not implemented");
+    return Promise.resolve("");
+  }
+
+  stream(): ReadableStream {
+    throw new Error("not implemented");
+    return new ReadableStream();
+  }
+
+  arrayBuffer(): Promise<ArrayBuffer> {
+    throw new Error("not implemented");
+    return Promise.resolve(new ArrayBuffer(0));
+  }
+
+  json(): Promise<any> {
+    throw new Error("not implemented");
+    return Promise.resolve({});
+  }
+
+  writer(params: { highWaterMark?: number }): FileSink {
+    throw new Error("not implemented");
+    return new BunFileSink();
+  }
+
+  exists(): Promise<boolean> {
+    throw new Error("not implemented");
+    return Promise.resolve(false);
+  }
+}
+
+class BunFileSink implements FileSink {
+  write(
+    chunk: string | ArrayBufferView | ArrayBuffer | SharedArrayBuffer
+  ): number {
+    throw new Error("not implemented");
+    return 0;
+  }
+
+  flush(): number | Promise<number> {
+    throw new Error("not implemented");
+    return 0;
+  }
+
+  end(error?: Error): number | Promise<number> {
+    throw new Error("not implemented");
+    return 0;
+  }
+
+  start(options?: { highWaterMark?: number }): void {
+    throw new Error("not implemented");
+  }
+
+  ref(): void {
+    throw new Error("not implemented");
+  }
+
+  unref(): void {
+    throw new Error("not implemented");
+  }
+}
