09e49b7b1574c88ea0003ee467c9b22fef1c828b
Spirobel <spirobel@protonmail.com>
2025-12-19 22:34:59 +0800

    openWallet(primary address) function that will open and scan a wallet with just the primary address provided. Run scripts/stagenet_gen.ts  before that to write a fresh stagenet spend and viewkey to .env + a default Scansettings.json. openwallet will open this file by convention and load the keys from env if nothing besides the primary address is provided. (it only makes sense to diverge from the convention for adhoc testing of a local dev wallet) Bun.file and Bun.write can be filled in for any platform you are targeting. (react native, browser, [bun already has them])

---
+++ typescript/wallet-api/scanning-syncing/openWallet.ts
@@ -0,0 +1,31 @@
+import {
+  ScanCacheOpened,
+  type ScanCacheOpenedCreateParams,
+} from "./scanCacheOpened";
+import type { ScanCache } from "./scanWithCache";
+export async function openWallets() {}
+/**
+ * Opens a **single wallet** for scanning.
+ * **Touches ScanSettings.json** (use `openWallets()` for multiple wallets).
+ *
+ * Scan runs in a worker thread to not block the main thread.
+ *
+ * Supports `pause()`, `unpause(node_url)`, and `wallet.node_url = "new_url"` mid-scan.
+ *
+ * @param primary_address Wallet address
+ * @param options Configuration options
+ * @returns Promise<ScanCacheOpened>
+ */
+export async function openWallet(
+  primary_address: string,
+  options: Omit<ScanCacheOpenedCreateParams, "primary_address" | "cache"> & {
+    cache?: ScanCache | string | true;
+  } = {}
+): Promise<ScanCacheOpened> {
+  return await ScanCacheOpened.create({
+    primary_address,
+    ...options,
+    cache: options.cache ?? true, // read from ScanSettings.json (default behavior),
+    // if cache instance is provided, in memory only. if path is provided, read from file, both ignore ScanSettings.json
+  } as ScanCacheOpenedCreateParams);
+}
