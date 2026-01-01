296cb42131b18ea7ecf0efe6325c4fd451818846
Spirobel <spirobel@protonmail.com>
2026-01-01 21:56:36 +0800

    add command to inline + checksum worker source. this is necessary for hassle free running of workers. Otherwise consumers of this library would have to add copying of files to their build process. (similar to the wasm file, where we circumvented this issue with the same approach. No assumptions of library consumer build pipeline is accomplished with this approach)

--- typescript/templates/inlineMacro.ts
+++ typescript/templates/inlineMacro.ts
@@ -1,5 +1,16 @@
 import * as fs from "fs";
+import { workerMainCode } from "../wallet-api/scanning-syncing/worker-entrypoints/worker";
 export function fileToBase64(filepath: string): string {
   const source = fs.readFileSync(filepath);
   return source.toString("base64");
+}
+
+export function workerSourceToBase64(workerSourceCode: string): string {
+  return Buffer.from(workerSourceCode, "utf8").toString("base64");
+}
+
+export function workerMainCodeToBase64(): string {
+  if (!workerMainCode)
+    throw new Error("workerSourceCode compilation did not work");
+  return workerSourceToBase64(workerMainCode);
 }
---
+++ typescript/templates/workers/worker.ts
@@ -0,0 +1,4 @@
+import { workerMainCodeToBase64 } from '../inlineMacro' with { type: 'macro' };
+const source = workerMainCodeToBase64()
+export const workerMainCode = new Uint8Array(atob(source).split("").map(function(c) {
+    return c.charCodeAt(0); }));
