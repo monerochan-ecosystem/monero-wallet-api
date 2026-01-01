7ad744f8325ba9a8f5dc19a78d32e0dab5c2c273
Spirobel <spirobel@protonmail.com>
2026-01-01 20:48:46 +0800

    get rid of worker multiple , worker.ts can scan multiple wallets now

--- typescript/wallet-api/scanning-syncing/worker-mains/workerMultiple.ts
+++
@@ -1,8 +0,0 @@
-import { scanMultipleWallets } from "../backgroundWorker";
-import type { ScanSettings } from "../scanSettings";
-declare const scan_settings: ScanSettings;
-
-await scanMultipleWallets(
-  (x) => self.postMessage({ type: "RESULT", payload: x }),
-  scan_settings
-);
