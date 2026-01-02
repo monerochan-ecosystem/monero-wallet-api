33d59a180f0ead4d2c7765eb65ce13f9651594c3
Spirobel <spirobel@protonmail.com>
2026-01-02 13:00:36 +0800

    indexedb writes  are atomic already

--- typescript/wallet-api/io/atomicWrite.ts
+++ typescript/wallet-api/io/atomicWrite.ts
@@ -6,8 +6,9 @@ export async function atomicWrite(
   targetPath: string,
   data: string | Blob | ArrayBuffer | SharedArrayBuffer | TypedArray | Response
 ): Promise<number> {
+  // in the browser we don't have rename + indexedDB writes are atomic in any case
+  if (!rename) return await Bun.write(targetPath, data as BunFile);
   const tempPath = targetPath + ".tmp";
-
   const bytesWritten = await Bun.write(tempPath, data as BunFile);
   await rename(tempPath, targetPath);
 
