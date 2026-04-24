cdbea913d020ba4787ef2a47113dfd05cb5eab33
Spirobel <spirobel@protonmail.com>
2026-04-24 15:39:33 +0800

    dont throw on partial atomic write

--- typescript/wallet-api/io/atomicWrite.ts
+++ typescript/wallet-api/io/atomicWrite.ts
@@ -3,7 +3,7 @@ import type { TypedArray } from "./BunFileInterface";
 
 export async function atomicWrite(
   targetPath: string,
-  data: string | Blob | ArrayBuffer | SharedArrayBuffer | TypedArray | Response
+  data: string | Blob | ArrayBuffer | SharedArrayBuffer | TypedArray | Response,
 ): Promise<number> {
   // in the browser we don't have rename + indexedDB writes are atomic in any case
   if (areWeInTheBrowser) return await Bun.write(targetPath, data as BunFile);
@@ -11,7 +11,11 @@ export async function atomicWrite(
 
   const tempPath = targetPath + ".tmp";
   const bytesWritten = await Bun.write(tempPath, data as BunFile);
-  await rename(tempPath, targetPath);
+  try {
+    await rename(tempPath, targetPath);
+  } catch (e) {
+    //eat rename failed
+  }
 
   return bytesWritten;
 }
