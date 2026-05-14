9eb8b3eb7450c8ebde872a6821baeeffcb511812
Spirobel <spirobel@protonmail.com>
2026-05-15 03:10:37 +0800

    add multisig worker main

---
+++ typescript/wallet-api/scanning-syncing/worker-mains/multisig-main.ts
@@ -0,0 +1,39 @@
+import {
+  DistributedKeyGenerator,
+  type DkgParticipateParams,
+  type DkgVerifyParams,
+} from "../../api";
+import { log } from "../../io/logging";
+export type MultiSigParticipateCall = {
+  type: "multisig-call";
+  functionName: "participate";
+  params: DkgParticipateParams;
+};
+export type MultiSigVerifyCallMsg = {
+  type: "multisig-call";
+  functionName: "verify";
+  params: DkgVerifyParams;
+};
+export type MultiSigWorkerCallMsg =
+  | MultiSigParticipateCall
+  | MultiSigVerifyCallMsg;
+
+export function multisigMainWorkerCall(
+  msg: MultiSigWorkerCallMsg,
+  dkg?: DistributedKeyGenerator,
+) {
+  if (!dkg) {
+    const error = new Error(
+      "multisigMainWorkerCall called without setup DistributedKeyGenerator first",
+    );
+    console.error("[multisigMainWorkerCall] error:", error);
+    log("multisigMainWorkerCall", ["error:", error]);
+
+    self.postMessage({ type: "ERROR", payload: error });
+  } else if (msg.type === "multisig-call") {
+    const functionName = msg.functionName;
+    const params = msg.params;
+    const result = dkg[functionName](params as any);
+    self.postMessage({ type: "multisig-call-result", result });
+  }
+}
